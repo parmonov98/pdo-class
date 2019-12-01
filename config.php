77bae131fa253b340cdf2b27665347d3dafb66b7
Murod <parmonov98@yandex.ru>
2019-12-01 08:51:15 +0500

    config vs connection
    config vs connection classes added

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DB_SERV", "127.0.0.1"); // host
+define("DB_NAME", "u536707397_pars");  // DB
+define("DB_USER", "u536707397_pars");  // usersame
+define("DB_PASS", "dK7Dfii0YRAp!M/!so"); // password
+
